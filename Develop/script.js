9b12350ce515a1809f02036ac44cfb10a3bfcb7c
Mabsky <grimdango@gmail.com>
2022-01-10 11:02:17 +1100

    2nd push

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,4 @@
-
+//list of hours
 var hour_9am = (moment('9:00am', 'hh:mm A'));
 var hour_10am = (moment('10:00am', 'hh:mm A'));
 var hour_11am = (moment('11:00am', 'hh:mm A'));
@@ -10,31 +10,9 @@ var hour_4pm = (moment('4:00pm', 'hh:mm A'));
 var hour_5pm = (moment('5:00pm', 'hh:mm A'));
 var hour_6pm = (moment('6:00pm', 'hh:mm A'));
 
-
-document.getElementById("currentDay").innerHTML = moment();
+//set format of time display
+document.getElementById("currentDay").innerHTML = moment().format('Do MMM YYYY');
 var d1 = moment().calendar(); 
-
-hours = {
-//moment instances of hours
-hour_9am: (moment('9:00am', 'hh:mm A')),
-hour_10am:(moment('10:00am', 'hh:mm A')),
-hour_11am: (moment('11:00am', 'hh:mm A')),
-hour_12pm: (moment('12:00pm', 'hh:mm A')),
-hour_1pm: (moment('01:00pm', 'hh:mm A')),
-hour_2pm:(moment('2:00pm', 'hh:mm A')),
-hour_3pm: (moment('3:00pm', 'hh:mm A')),
-hour_4pm: (moment('4:00pm', 'hh:mm A')),
-hour_5pm: (moment('5:00pm', 'hh:mm A')),
-hour_6pm: (moment('6:00pm', 'hh:mm A')),
-}
-//add hours to hour_list in html
-
-//for (i=0;i<9;i++) {
-//    document.querySelector(".hour_list").innerHTML += hour;
-//}
-
-
-
 
 //set color scheme for all hours
 if (moment().isBefore(hour_9am)) {
@@ -110,30 +88,239 @@ if (moment().isBefore(hour_5pm)) {
     document.getElementById("hour_5pm").style.backgroundColor="LightPink";
 }
 
+//9am Row Function
+var task_9am = document.querySelector(".write_task_9am");
+var checkbox_9am = document.querySelector(".save_task_9am");
 
-//set 
-var task = document.querySelector(".write_task");
-//var tasks = $(".write_task")
-
-//set checkbox save function
-var checkbox = document.querySelector(".save_task");
-//var checkboxes = $(".save_task").bind(save_checkboxes);
-
-var time_rows = document.getElementsByTagName("time_row")
-
-
-function save_checkboxes() {
-    if (checkbox.checked = true) {
-        localStorage.setItem("task", task.value)
+function save_checkboxes_9am() {
+    if (checkbox_9am.checked = true) {
+        localStorage.setItem("task_9am", task_9am.value)
     } else if (checkbox.checked = false) {
-        localStorage.removeItem("task", task.value)
+        localStorage.removeItem("task_9am", task_9am.value)
+    }
+}
+
+function retrieve_data_9am() {
+    task_9am.value = localStorage.getItem("task_9am", task_9am.value);
+}
+
+checkbox_9am.addEventListener('change', save_checkboxes_9am);
+
+function save_checkboxes_9am() {
+    if (checkbox_9am.checked = true) {
+        localStorage.setItem("task_9am", task_9am.value)
+    } else if (checkbox.checked = false) {
+        localStorage.removeItem("task_9am", task_9am.value)
+    }
+}
+
+//10am Row Function
+var task_10am = document.querySelector(".write_task_10am");
+var checkbox_10am = document.querySelector(".save_task_10am");
+
+function save_checkboxes_10am() {
+    if (checkbox_10am.checked = true) {
+        localStorage.setItem("task_10am", task_10am.value)
+    } else if (checkbox.checked = false) {
+        localStorage.removeItem("task_10am", task_10am.value)
+    }
+}
+
+function retrieve_data_10am() {
+    task_10am.value = localStorage.getItem("task_10am", task_10am.value);
+}
+
+checkbox_10am.addEventListener('change', save_checkboxes_10am);
+
+function save_checkboxes_10am() {
+    if (checkbox_10am.checked = true) {
+        localStorage.setItem("task_10am", task_10am.value)
+    } else if (checkbox.checked = false) {
+        localStorage.removeItem("task_10am", task_10am.value)
+    }
+}
+
+//11am Row Function
+var task_11am = document.querySelector(".write_task_11am");
+var checkbox_11am = document.querySelector(".save_task_11am");
+
+function save_checkboxes_11am() {
+    if (checkbox_11am.checked = true) {
+        localStorage.setItem("task_11am", task_11am.value)
+    } else if (checkbox.checked = false) {
+        localStorage.removeItem("task_11am", task_11am.value)
+    }
+}
+
+function retrieve_data_11am() {
+    task_11am.value = localStorage.getItem("task_11am", task_11am.value);
+}
+
+checkbox_11am.addEventListener('change', save_checkboxes_11am);
+
+function save_checkboxes_11am() {
+    if (checkbox_11am.checked = true) {
+        localStorage.setItem("task_11am", task_11am.value)
+    } else if (checkbox.checked = false) {
+        localStorage.removeItem("task_11am", task_11am.value)
     }
 }
 
 
-function retrieve_data() {
-    task.value = localStorage.getItem("task", task.value);
+//12pm Row Function
+var task_12pm = document.querySelector(".write_task_12pm");
+var checkbox_12pm = document.querySelector(".save_task_12pm");
 
+function save_checkboxes_12pm() {
+    if (checkbox_12pm.checked = true) {
+        localStorage.setItem("task_12pm", task_12pm.value)
+    } else if (checkbox.checked = false) {
+        localStorage.removeItem("task_12pm", task_12pm.value)
+    }
+}
+
+function retrieve_data_12pm() {
+    task_12pm.value = localStorage.getItem("task_12pm", task_12pm.value);
+}
+
+checkbox_12pm.addEventListener('change', save_checkboxes_12pm);
+
+function save_checkboxes_12pm() {
+    if (checkbox_12pm.checked = true) {
+        localStorage.setItem("task_12pm", task_12pm.value)
+    } else if (checkbox.checked = false) {
+        localStorage.removeItem("task_12pm", task_12pm.value)
+    }
+}
+
+//1pm Row Function
+var task_1pm = document.querySelector(".write_task_1pm");
+var checkbox_1pm = document.querySelector(".save_task_1pm");
+
+function save_checkboxes_1pm() {
+    if (checkbox_1pm.checked = true) {
+        localStorage.setItem("task_1pm", task_1pm.value)
+    } else if (checkbox.checked = false) {
+        localStorage.removeItem("task_1pm", task_1pm.value)
+    }
+}
+
+function retrieve_data_1pm() {
+    task_1pm.value = localStorage.getItem("task_1pm", task_1pm.value);
+}
+
+checkbox_1pm.addEventListener('change', save_checkboxes_1pm);
+
+function save_checkboxes_1pm() {
+    if (checkbox_1pm.checked = true) {
+        localStorage.setItem("task_1pm", task_1pm.value)
+    } else if (checkbox.checked = false) {
+        localStorage.removeItem("task_1pm", task_1pm.value)
+    }
+}
+
+//2pm Row Function
+var task_2pm = document.querySelector(".write_task_2pm");
+var checkbox_2pm = document.querySelector(".save_task_2pm");
+
+function save_checkboxes_2pm() {
+    if (checkbox_2pm.checked = true) {
+        localStorage.setItem("task_2pm", task_2pm.value)
+    } else if (checkbox.checked = false) {
+        localStorage.removeItem("task_2pm", task_2pm.value)
+    }
+}
+
+function retrieve_data_2pm() {
+    task_2pm.value = localStorage.getItem("task_2pm", task_2pm.value);
+}
+
+checkbox_2pm.addEventListener('change', save_checkboxes_2pm);
+
+function save_checkboxes_2pm() {
+    if (checkbox_2pm.checked = true) {
+        localStorage.setItem("task_2pm", task_2pm.value)
+    } else if (checkbox.checked = false) {
+        localStorage.removeItem("task_2pm", task_2pm.value)
+    }
+}
+
+//3pm Row Function
+var task_3pm = document.querySelector(".write_task_3pm");
+var checkbox_3pm = document.querySelector(".save_task_3pm");
+
+function save_checkboxes_3pm() {
+    if (checkbox_3pm.checked = true) {
+        localStorage.setItem("task_3pm", task_3pm.value)
+    } else if (checkbox.checked = false) {
+        localStorage.removeItem("task_3pm", task_3pm.value)
+    }
+}
+
+function retrieve_data_3pm() {
+    task_3pm.value = localStorage.getItem("task_3pm", task_3pm.value);
+}
+
+checkbox_3pm.addEventListener('change', save_checkboxes_3pm);
+
+function save_checkboxes_3pm() {
+    if (checkbox_3pm.checked = true) {
+        localStorage.setItem("task_3pm", task_3pm.value)
+    } else if (checkbox.checked = false) {
+        localStorage.removeItem("task_3pm", task_3pm.value)
+    }
+}
+
+//4pm Row Function
+var task_4pm = document.querySelector(".write_task_4pm");
+var checkbox_4pm = document.querySelector(".save_task_4pm");
+
+function save_checkboxes_4pm() {
+    if (checkbox_4pm.checked = true) {
+        localStorage.setItem("task_4pm", task_4pm.value)
+    } else if (checkbox.checked = false) {
+        localStorage.removeItem("task_4pm", task_4pm.value)
+    }
+}
+
+function retrieve_data_4pm() {
+    task_4pm.value = localStorage.getItem("task_4pm", task_4pm.value);
+}
+
+checkbox_4pm.addEventListener('change', save_checkboxes_4pm);
+
+function save_checkboxes_4pm() {
+    if (checkbox_4pm.checked = true) {
+        localStorage.setItem("task_4pm", task_4pm.value)
+    } else if (checkbox.checked = false) {
+        localStorage.removeItem("task_4pm", task_4pm.value)
+    }
+}
+
+//5pm Row Function
+var task_5pm = document.querySelector(".write_task_5pm");
+var checkbox_5pm = document.querySelector(".save_task_5pm");
+
+function save_checkboxes_5pm() {
+    if (checkbox_5pm.checked = true) {
+        localStorage.setItem("task_5pm", task_5pm.value)
+    } else if (checkbox.checked = false) {
+        localStorage.removeItem("task_5pm", task_5pm.value)
+    }
+}
+
+function retrieve_data_5pm() {
+    task_5pm.value = localStorage.getItem("task_5pm", task_5pm.value);
+}
+
+checkbox_5pm.addEventListener('change', save_checkboxes_5pm);
+
+function save_checkboxes_5pm() {
+    if (checkbox_5pm.checked = true) {
+        localStorage.setItem("task_5pm", task_4pm.value)
+    } else if (checkbox.checked = false) {
+        localStorage.removeItem("task_5pm", task_4pm.value)
+    }
 }
 
 
@@ -142,12 +329,15 @@ function retrieve_data() {
 
 
 
-
-checkbox.addEventListener('change', save_checkboxes);
-
-window.addEventListener("load", retrieve_data);
-
-
+window.addEventListener("load", retrieve_data_9am);
+window.addEventListener("load", retrieve_data_10am);
+window.addEventListener("load", retrieve_data_11am);
+window.addEventListener("load", retrieve_data_12pm);
+window.addEventListener("load", retrieve_data_1pm);
+window.addEventListener("load", retrieve_data_2pm);
+window.addEventListener("load", retrieve_data_3pm);
+window.addEventListener("load", retrieve_data_4pm);
+window.addEventListener("load", retrieve_data_5pm);
 
 //window.addEventListener("load", retrieve_data );
 
--- README.md
+++ README.md
@@ -1,89 +1,25 @@
-# 05 Third-Party APIs: Work Day Scheduler
+# Day Planner
 
-## Your Task
+## Summary
 
-Create a simple calendar application that allows a user to save events for each hour of the day by modifying starter code. This app will run in the browser and feature dynamically updated HTML and CSS powered by jQuery.
+Day planner utilising moment.js to color-code time data.
 
-You'll need to use the [Moment.js](https://momentjs.com/) library to work with date and time. Be sure to read the documentation carefully and concentrate on using Moment.js in the browser.
+Forms are provided for writing down tasks, and checkboxes for saving into local storage. 
 
-## User Story
+## Description
 
-```md
-AS AN employee with a busy schedule
-I WANT to add important events to a daily planner
-SO THAT I can manage my time effectively
-```
+Upon loading: The webpage will show a Work Day Scheduler title, subheader, and current date. 
 
-## Acceptance Criteria
-
-```md
-GIVEN I am using a daily planner to create a schedule
-WHEN I open the planner
-THEN the current day is displayed at the top of the calendar
-WHEN I scroll down
-THEN I am presented with timeblocks for standard business hours
-WHEN I view the timeblocks for that day
-THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-WHEN I click into a timeblock
-THEN I can enter an event
-WHEN I click the save button for that timeblock
-THEN the text for that event is saved in local storage
-WHEN I refresh the page
-THEN the saved events persist
-```
-
-The following animation demonstrates the application functionality:
-
-![A user clicks on slots on the color-coded calendar and edits the events.](./Assets/05-third-party-apis-homework-demo.gif)
+Below this will be a table of rows indicating columns for Time, Task, and Save.
 
 
-## Grading Requirements
 
-This homework is graded based on the following criteria: 
 
-### Technical Acceptance Criteria: 40%
 
-* Satisfies all of the above acceptance criteria plus the following:
+## Log
 
-  * Uses a date utility library to work with date and time
+### 09/01/22
 
-### Deployment: 32%
+First upload
 
-* Application deployed at live URL
-
-* Application loads with no errors
-
-* Application GitHub URL submitted
-
-* GitHub repo contains application code
-
-### Application Quality: 15%
-
-* Application user experience is intuitive and easy to navigate
-
-* Application user interface style is clean and polished
-
-* Application resembles the mock-up functionality provided in the homework instructions
-
-### Repository Quality: 13%
-
-* Repository has a unique name
-
-* Repository follows best practices for file structure and naming conventions
-
-* Repository follows best practices for class/id naming conventions, indentation, quality comments, etc.
-
-* Repository contains multiple descriptive commit messages
-
-* Repository contains quality README file with description, screenshot, and link to deployed application
-
-## Review
-
-You are required to submit the following for review:
-
-* The URL of the deployed application
-
-* The URL of the GitHub repository, with a unique name and a README describing the project
-
-- - -
-© 2021 Trilogy Education Services, LLC, a 2U, Inc. brand. Confidential and Proprietary. All Rights Reserved.
+Need to install loop: 
